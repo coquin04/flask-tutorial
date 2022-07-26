5381dac7d84c3600a47b98131d9a04a37913b9c2
jorge-juareza <coquin04@gmail.com>
2022-07-26 11:30:17 -0500

    Add: Setup.py to make project installable

---
+++ flaskr/setup.py
@@ -0,0 +1,13 @@
+from xml.etree.ElementPath import find
+from setuptools import find_packages, setup
+
+setup(
+    name='flask',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
